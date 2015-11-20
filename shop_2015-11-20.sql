faafdc8a25bb030521275d06b060e3d8fe3283c4
Ross E <ross.eastman@mayden.co.uk>
2015-11-20 14:48:39 +0000

    #mobSynergy - completed (fur now)the db wiv da mob *powpow*

--- shop_2015-11-20.sql
+++ shop_2015-11-20.sql
@@ -7,9 +7,7 @@
 #
 # Host: 192.168.20.56 (MySQL 5.6.25)
 # Database: shop
-
-# Generation Time: 2015-11-20 12:09:32 +0000
-
+# Generation Time: 2015-11-20 14:47:37 +0000
 # ************************************************************
 
 
@@ -22,25 +20,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-
 # Dump of table address
 # ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `address`;
 
 CREATE TABLE `address` (
   `id` int(11) unsigned NOT NULL,
   `user_id` int(11) unsigned NOT NULL,
+  `supplier_id` int(11) unsigned NOT NULL,
   `organisation` varchar(60) DEFAULT NULL,
   `address_line_1` varchar(50) NOT NULL,
   `address_line_2` varchar(60) DEFAULT NULL,
   `town` varchar(35) NOT NULL,
   `county` varchar(20) NOT NULL,
-  `post_code` varchar(7) NOT NULL,
+  `post_code` varchar(8) NOT NULL DEFAULT '',
   `country` varchar(50) NOT NULL,
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_last_updated` datetime DEFAULT CURRENT_TIMESTAMP,
+  `default` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 
 # Dump of table order
 # ------------------------------------------------------------
@@ -52,10 +54,52 @@ CREATE TABLE `order` (
   `user_id` int(11) unsigned NOT NULL,
   `status` varchar(100) NOT NULL,
   `order_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `retail_price` decimal(7,2) NOT NULL,
-  `order_total` decimal(4,2) NOT NULL DEFAULT '20.00',
+  `retail_price` decimal(20,2) NOT NULL,
+  `order_total` decimal(20,2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table order_item
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `order_item`;
+
+CREATE TABLE `order_item` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) unsigned NOT NULL,
+  `product_id` int(11) unsigned NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `discount` int(11) DEFAULT NULL,
+  `price_per_item` int(11) DEFAULT NULL,
+  `item_total` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table payment
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `payment`;
+
+CREATE TABLE `payment` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `order_id` int(11) unsigned NOT NULL,
+  `bank_name` varchar(50) DEFAULT NULL,
+  `name_on_card` varchar(50) NOT NULL,
+  `credit_card_number` char(4) NOT NULL,
+  `payment_method` enum('paypal','card','nutz','voucher','soul','groupon') NOT NULL DEFAULT 'nutz',
+  `start_date` char(7) NOT NULL,
+  `expiry_date` char(7) NOT NULL,
+  `security_code` char(3) NOT NULL,
+  `sort_code` char(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 
 # Dump of table product
 # ------------------------------------------------------------
@@ -69,16 +113,32 @@ CREATE TABLE `product` (
   `stock_level` int(11) NOT NULL,
   `description` varchar(500) DEFAULT NULL,
   `name` varchar(100) NOT NULL,
-  `vat_rate` decimal(4,2) NOT NULL DEFAULT '20.00',
+  `vat_rate` decimal(4,2) unsigned NOT NULL DEFAULT '20.00',
   `image` blob,
   `size` varchar(10) DEFAULT NULL,
   `category_id` varchar(50) NOT NULL,
-  `weight_grams` int(7) DEFAULT NULL,
-  `height_cm` int(7) DEFAULT NULL,
-  `width_cm` int(7) DEFAULT NULL,
-  `length_cm` int(7) DEFAULT NULL,
+  `weight_grams` int(7) unsigned DEFAULT NULL,
+  `height_cm` int(7) unsigned DEFAULT NULL,
+  `width_cm` int(7) unsigned DEFAULT NULL,
+  `length_cm` int(7) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8  ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table product_supplier
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `product_supplier`;
+
+CREATE TABLE `product_supplier` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) unsigned NOT NULL,
+  `supplier_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 
 # Dump of table review
 # ------------------------------------------------------------
@@ -90,10 +150,13 @@ CREATE TABLE `review` (
   `user_id` int(11) unsigned NOT NULL,
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_last_updated` datetime DEFAULT CURRENT_TIMESTAMP,
-  `description` varchar(500) DEFAULT NULL,
-  `rating` tinyint(2) NOT NULL,
+  `title` varchar(50) NOT NULL DEFAULT '',
+  `body` text NOT NULL,
+  `rating` tinyint(2) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 
 # Dump of table supplier
 # ------------------------------------------------------------
@@ -109,8 +172,11 @@ CREATE TABLE `supplier` (
   `website` varchar(320) DEFAULT NULL,
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_last_updated` datetime DEFAULT CURRENT_TIMESTAMP,
+  `description` varchar(500) DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 
 # Dump of table user
 # ------------------------------------------------------------
@@ -120,47 +186,21 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
-  `password` char(40) NOT NULL,
   `salt` char(40) NOT NULL,
+  `password` char(40) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(320) NOT NULL DEFAULT '',
   `telephone` varchar(12) DEFAULT NULL,
+  `mobile` varchar(12) DEFAULT NULL,
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_last_updated` datetime DEFAULT CURRENT_TIMESTAMP,
+  `email_verified` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `order_item`;
-CREATE TABLE `order_item` (
-`id` INT(11) NOT NULL AUTO_INCREMENT,
-`order_id` INT(11) NOT NULL,
-`product_id` INT(11) NOT NULL,
-`quantity` INT(11) NOT NULL,
-`discount` INT(11),
-`price_per_item` INT(11),
-`item_total` INT(11),
-PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-# Dump of table payment
-# ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `payment`;
-
-CREATE TABLE `payment` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) unsigned NOT NULL,
-  `order_id` int(11) unsigned NOT NULL,
-  `bank_name` VARCHAR(50),
-  `name_on_card` VARCHAR(50) NOT NULL,
-  `credit_card_number` CHAR(4) NOT NULL,
-  `start_date` CHAR(7) NOT NULL,
-  `expiry_date` CHAR(7) NOT NULL,
-  `security_code` CHAR(3) NOT NULL,
-  `sort_code` CHAR(8) NOT NULL,
-  PRIMARY KEY (`id`)
-);
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
