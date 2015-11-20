95b1100038bf27a091f701bdcb4b654a5927104a
charliecog <charlie.coggans@mayden.co.uk>
2015-11-20 12:00:33 +0000

    updated from develop

--- shop_2015-11-20.sql
+++ shop_2015-11-20.sql
@@ -7,9 +7,7 @@
 #
 # Host: 192.168.20.56 (MySQL 5.6.25)
 # Database: shop
-
-# Generation Time: 2015-11-20 11:15:50 +0000
-
+# Generation Time: 2015-11-20 12:00:04 +0000
 # ************************************************************
 
 
@@ -22,41 +20,24 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-# Dump of table review
+# Dump of table user
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `review`;
+DROP TABLE IF EXISTS `user`;
 
-CREATE TABLE `product` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cost_price` decimal(7,2) NOT NULL,
-  `retail_price` decimal(7,2) NOT NULL,
-  `stock_level` int(11) NOT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  `name` varchar(100) NOT NULL,
-  `vat_rate` decimal(4,2) NOT NULL DEFAULT '20.00',
-  `image` blob,
-  `size` varchar(10) DEFAULT NULL,
-  `category_id` varchar(50) NOT NULL,
-  `weight_grams` int(7) DEFAULT NULL,
-  `height_cm` int(7) DEFAULT NULL,
-  `width_cm` int(7) DEFAULT NULL,
-  `length_cm` int(7) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` char(40) NOT NULL,
+  `salt` char(40) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(320) DEFAULT NULL,
+  `telephone` varchar(12) DEFAULT NULL,
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_last_updated` datetime DEFAULT CURRENT_TIMESTAMP,
-   PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `review` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `date_last_updated` datetime DEFAULT CURRENT_TIMESTAMP,
-  `description` varchar(500) DEFAULT NULL,
-  `rating` tinyint(2) NOT NULL,
-   PRIMARY KEY (`id`)
-);
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
