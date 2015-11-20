cbbfe903c381139911bc2b0fc73481d75dcfb908
charliecog <charlie.coggans@mayden.co.uk>
2015-11-20 12:11:57 +0000

    hotfix on syntax, #synergy.

--- shop_2015-11-20.sql
+++ shop_2015-11-20.sql
@@ -1,21 +1,30 @@
-DROP TABLE IF EXISTS `product`;
-CREATE TABLE `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cost_price` decimal(7,2) NOT NULL,
-  `retail_price` decimal(7,2) NOT NULL,
-  `stock_level` int(11) NOT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  `name` varchar(100) NOT NULL,
-  `vat_rate` decimal(4,2) NOT NULL DEFAULT '20.00',
-  `image` blob,
-  `size` varchar(10) DEFAULT NULL,
-  `category_id` varchar(50) NOT NULL,
-  `weight_grams` int(7) DEFAULT NULL,
-  `height_cm` int(7) DEFAULT NULL,
-  `width_cm` int(7) DEFAULT NULL,
-  `length_cm` int(7) DEFAULT NULL,
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 192.168.20.56 (MySQL 5.6.25)
+# Database: shop
+# Generation Time: 2015-11-20 12:11:41 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table address
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `address`;
+
 CREATE TABLE `address` (
   `id` int(11) unsigned NOT NULL,
   `user_id` int(11) unsigned NOT NULL,
@@ -31,19 +40,13 @@ CREATE TABLE `address` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `review`;
-CREATE TABLE `review` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `date_last_updated` datetime DEFAULT CURRENT_TIMESTAMP,
-  `description` varchar(500) DEFAULT NULL,
-  `rating` tinyint(2) NOT NULL,
-   PRIMARY KEY (`id`)
-);
+
+
+# Dump of table order
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `order`;
+
 CREATE TABLE `order` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
@@ -52,9 +55,58 @@ CREATE TABLE `order` (
   `retail_price` decimal(7,2) NOT NULL,
   `order_total` decimal(4,2) NOT NULL DEFAULT '20.00',
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table product
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cost_price` decimal(7,2) NOT NULL,
+  `retail_price` decimal(7,2) NOT NULL,
+  `stock_level` int(11) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `name` varchar(100) NOT NULL,
+  `vat_rate` decimal(4,2) NOT NULL DEFAULT '20.00',
+  `image` blob,
+  `size` varchar(10) DEFAULT NULL,
+  `category_id` varchar(50) NOT NULL,
+  `weight_grams` int(7) DEFAULT NULL,
+  `height_cm` int(7) DEFAULT NULL,
+  `width_cm` int(7) DEFAULT NULL,
+  `length_cm` int(7) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table review
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `review`;
+
+CREATE TABLE `review` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_last_updated` datetime DEFAULT CURRENT_TIMESTAMP,
+  `description` varchar(500) DEFAULT NULL,
+  `rating` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table user
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
@@ -68,3 +120,13 @@ CREATE TABLE `user` (
   `date_last_updated` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
