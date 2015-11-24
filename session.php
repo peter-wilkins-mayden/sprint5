1572334697ef5746375aa1fcdbe76871daa6648c
charliecog <charlie.coggans@mayden.co.uk>
2015-11-24 20:29:25 +0000

    simplefront end

---
+++ session.php
@@ -0,0 +1,47 @@
+<?php
+
+$dsn = 'mysql:host=shop.c2g7heuzpzpp.eu-west-1.rds.amazonaws.com;port=3306;dbname=shop';
+$user = 'liveShop';
+$password = 'AWSLiveShop';
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    echo 'Connected!';
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+/*$username = $_POST['username'];
+$password = $_POST['password'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$mobile = $_POST['mobile'];*/
+
+$sql = "INSERT INTO user (
+                        `username`,
+                        `password`,
+                        `first_name`,
+                        `last_name`,
+                        `email`,
+                        `mobile`)
+                 VALUES (
+                        :username,
+                        :password,
+                        :first_name,
+                        :last_name,
+                        :email,
+                        :mobile)";
+
+$result = $pdo->prepare($sql);
+$result->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
+$result->bindParam(':password,', $_POST['password'], PDO::PARAM_STR);
+$result->bindParam(':first_name', $_POST['firstname'], PDO::PARAM_STR);
+$result->bindParam(':last_name', $_POST['lastname'], PDO::PARAM_STR);
+$result->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
+$result->bindParam(':mobile', $_POST['mobile'], PDO::PARAM_INT);
+$result->execute();
+
+
+
+echo 'testing';
