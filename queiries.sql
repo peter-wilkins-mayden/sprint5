69119a1ea3408233e8fd9afcfda5efaf9a97d1cf
Ross E <ross.eastman@mayden.co.uk>
2015-11-24 16:03:56 +0000

    wueries update

--- queiries.sql
+++ queiries.sql
@@ -2,15 +2,15 @@
 
 /*1. Insert new user.*/
 INSERT INTO user (`username`, `salt`, `password`, `first_name`,
-`last_name`,
-`email`,
-`telephone`,
-`mobile`)
+                  `last_name`,
+                  `email`,
+                  `telephone`,
+                  `mobile`)
 VALUES (:username, :salt, :password, :first_name,
-:last_name,
-:email,
-:telephone,
-:mobile);
+        :last_name,
+        :email,
+        :telephone,
+        :mobile);
 
 /*2. Authenticate/login user.*/
 SELECT  salt, password FROM user u
@@ -23,7 +23,7 @@ GROUP BY category;
 
 /*4. Show products with 5 star ratings ordered by popularity.*/
 SELECT product.name FROM product
-JOIN review ON product.id=review.product_id
+  JOIN review ON product.id=review.product_id
 WHERE rating=5
 GROUP BY product.name
 ORDER BY AVG(rating);
@@ -32,12 +32,22 @@ ORDER BY AVG(rating);
 SELECT image, description, order_status, order_id, order_date, delivery_address, total cost
 FROM `order`
 WHERE user_id=:user;
-/*6. List all items in a particular order showing photo, description, Quantity, cost per item, line total.*/
+/*6. List all items in a particular Order showing photo, description, Quantity, cost per item, line total.*/
 SELECT product.image, product.description, order_item.quantity,
-  order_item.cost_per_item, SUM(order_item.item_total)
-FROM product JOIN
-/*7. Show all products which are currently on offer (ordered by %age off) [if you have implemented offers]
-8. Search for all items in a particular category matching (or closely matching) my search key word(s)
+  order_item.price_per_item, order_total
+FROM product
+  JOIN order_item
+ON product_id = product.id
+JOIN `order`
+ON order_id = order.id
+WHERE order_id = 1;
+
+/*7. Show all products which are currently on offer (ordered by %age off) [if you have implemented offers]*/
+SELECT product.name, product.discount_price/product.retail_price*100 AS 'percentage'
+
+
+
+/*8. Search for all items in a particular category matching (or closely matching) my search key word(s)
 9. Recommend products to me which were bought by people who also purchased my last item.
 10. Show my items in a paricular category, between price range, with an average review rating of 3-4 stars.
 
