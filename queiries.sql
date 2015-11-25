4ab05f42b3b927a76a1a098d7d10efff79f0ffb1
Ross E <ross.eastman@mayden.co.uk>
2015-11-25 14:15:01 +0000

    updated queries

--- queiries.sql
+++ queiries.sql
@@ -13,56 +13,110 @@ VALUES (:username, :salt, :password, :first_name,
         :mobile);
 
 /*2. Authenticate/login user.*/
-SELECT  salt, password FROM user u
-WHERE username = :username;
+SELECT count(*)
+FROM user
+WHERE username = :username
+      AND password = sha1(concat(:salt, :password));
 
 /*3. List Categories, ordered by how many products in that category (highest first). */
 
-SELECT category, COUNT(product.id) FROM product
-GROUP BY category;
+SELECT
+  category,
+  COUNT(product.id) AS productCount
+FROM product
+GROUP BY category
+ORDER BY productCount DESC;
 
 /*4. Show products with 5 star ratings ordered by popularity.*/
-SELECT product.name FROM product
-  JOIN review ON product.id=review.product_id
-WHERE rating=5
+SELECT
+  product.name,
+  AVG(review.rating) AS avRating
+FROM product
+  JOIN review
+    ON product.id = review.product_id
 GROUP BY product.name
+HAVING avRating >= 4.5
 ORDER BY AVG(rating);
 
 /*5. List My Orders, showing image, description, order status, order_id, order_date, delivery_address & total cost.*/
-SELECT image, description, order_status, order_id, order_date, delivery_address, total cost
+SELECT
+  order.status,
+  order.id,
+  order_date,
+  address_line_1,
+  address_line_2,
+  town,
+  county,
+  post_code,
+  country,
+  order_total
 FROM `order`
-WHERE user_id=:user;
+  JOIN address
+    ON order.user_id = address.user_id
+WHERE `order`.user_id = :user;
 /*6. List all items in a particular Order showing photo, description, Quantity, cost per item, line total.*/
-SELECT product.image, product.description, order_item.quantity,
-  order_item.price_per_item, order_total
+SELECT
+  product.image,
+  product.description,
+  order_item.quantity,
+  order_item.price_per_item,
+  order_item.item_total
 FROM product
   JOIN order_item
-ON product_id = product.id
-JOIN `order`
-ON order_id = order.id
-WHERE order_id = 1;
+    ON product_id = product.id
+WHERE order_id = 2;
 
 /*7. Show all products which are currently on offer (ordered by %age off) [if you have implemented offers]*/
-SELECT product.name, ((product.retail_price-product.discount_price)/product.retail_price)*100 AS 'percentage'
+SELECT
+  product.name,
+  ((product.retail_price - product.discount_price) / product.retail_price) * 100 AS 'percentage'
 FROM product
-WHERE discount_price !=0
+WHERE discount_price != 0
 ORDER BY percentage DESC;
+.
 
 /*8. Search for all items in a particular category matching (or closely matching) my search key word(s)*/
-SELECT product.name, product.category
+SELECT
+  product.name,
+  product.category
 FROM product
 WHERE category SOUNDS LIKE (:category);
 /*GROUP BY product.name;*/
 
 /*9. Recommend products to me which were bought by people who also purchased my last item.*/
-SELECT product.name
-FROM product
-JOIN order_item
-ON product_id = product.id
-JOIN order
-ON order_id = order.id
-JOIN user
-ON user_id = user.id;
+SELECT user_id
+FROM order_item
+  JOIN `order`
+    ON order_item.order_id = order.id
+WHERE order_item.product_id = 1
+GROUP BY `order`.user_id;
+
+
+SELECT order_item.product_id, count(product_id) AS productCount, product.name
+FROM `order`
+  JOIN `order_item`
+    ON order_item.order_id = order.id
+  JOIN (SELECT user_id
+        FROM order_item
+          JOIN `order`
+            ON order_item.order_id = order.id
+        WHERE order_item.product_id = :lastProduct
+        GROUP BY `order`.user_id) AS specialUser
+    ON `order`.user_id = specialUser.user_id
+  JOIN product
+  ON product.id = order_item.product_id
+GROUP BY product_id
+ORDER BY productCount DESC;
+
+
+# product.name
+# FROM product
+# JOIN order_item
+# ON product_id = product.id
+#
+# JOIN user
+# ON user_id = user.id
+
 
 
 /*10. Show my items in a particular category, between price range, with an average review rating of 3-4 stars.
